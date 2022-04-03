84681860bee69a6266c94a8386d3f62b01435c72
Muhammad Hammad <heyhammadhere@gmail.com>
2022-04-03 22:01:17 +0500

    v1.0.3

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const bcrypt = require("bcryptjs");
 const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
+const helmet = require("helmet");
 const mongoose = require("mongoose");
-const morgan = require("morgan");
 const volleyball = require("volleyball");
 const PORT = process.env.PORT || 3000;
 
 // Routes
 const authRoute = require("./routes/auth");
 
-// mongoose.connect();
-
 // Middlewares
-app.use(morgan("dev"));
+app.use(express.json());
+app.use(helmet());
 app.use(volleyball);
 app.use(authRoute);
 
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,15 +1,33 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const Joi = require("joi");
 const { signUp, signIn } = require("./auth_constants");
 
-router.get(signUp, (req, res) => {
+const schema = Joi.object({
+  businessName: Joi.string().alphanum().min(3).max(50).required(),
+  firstName: Joi.string().alphanum().min(3).max(50).required(),
+  lastName: Joi.string().alphanum().min(3).max(50).required(),
+  email: Joi.string().email().required(),
+  contact: Joi.string()
+    .min(9)
+    .max(14)
+    .pattern(/^[0-9]*$/)
+    .required(),
+  country: Joi.string().min(3).max(50).required(),
+  password: Joi.string().alphanum().min(8).max(50).required(),
+});
+
+router.post(signUp, async (req, res) => {
   res.json({
-    message: signUp,
+    route: signUp,
+    message: req.body,
   });
 });
 
-router.get(signIn, (req, res) => {
+router.post(signIn, (req, res) => {
   res.json({
-    message: signIn,
+    route: signIn,
+    message: req.body,
   });
 });
 
